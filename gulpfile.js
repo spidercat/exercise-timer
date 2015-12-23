ed7877dfdf54af4514313efedba94c16ab263366
Laura <laura.kyle91@gmail.com>
2015-12-23 01:20:46 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prettify = require('gulp-html-prettify');
+
+gulp.task('styles', function () {
+  gulp.src('./src/styles.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', function () {
+  gulp.watch('./src/styles.scss', ['styles']);
+});
+
+gulp.task('templates', function () {
+  gulp.src('./*.html')
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(gulp.dest('./'));
+});

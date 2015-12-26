055060594bfb4d0057fca5d69e0e597c8aa49095
Laura <laura.kyle91@gmail.com>
2015-12-25 21:12:47 -0500

    Option count finished

--- src/app.js
+++ src/app.js
@@ -12,46 +12,91 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 	$scope.timeOn = ['00', ':', '20'];
 	$scope.timeOnJoined = $scope.timeOn.join('');
 	$scope.timeOffJoined = $scope.timeOff.join('');
-	 
+	
+	
+	
+	// Put 0 if digit less than 10
+	function minTwoDigits(n) {
+  	return (n < 10 ? '0' : '') + n;
+	}
+	
+	
 	$scope.changeRounds = function(value) {
 		// Hold parsed int
 		var numRounds = parseInt($scope.rounds);
-		if (value === 'minus') {
-			// Stops count from going below zero
-			if (numRounds != 0) {
-				$scope.rounds = numRounds - 1;
-			}
+		if ((value === 'minus') && (numRounds != 0)) {
+			$scope.rounds = numRounds - 1;
 		}
 		else if (value === 'plus') {
 			$scope.rounds = numRounds + 1;
 		}
 	} // End changeRounds()
 	
-	// Todo: If below 10, add 0 in front
+
 	$scope.changeTimeOn = function(value) {
 		var num = parseInt($scope.timeOn[2]);
+		var min = parseInt($scope.timeOn[0]);
 		if (value === 'minus') {
-			$scope.timeOn[2] = num - 1;
+			if (num == '00') {
+				if (min == '00') {
+					$scope.timeOff[0] = minTwoDigits(min);
+				}
+				else {
+					$scope.timeOn[2] = '59';
+					$scope.timeOn[0] = minTwoDigits(min - 1);
+				}
+			}
+			else {
+				$scope.timeOn[2] = minTwoDigits(num - 1);
+			}
 		}
 		else if (value === 'plus') {
-			$scope.timeOn[2] = num + 1;
+			if (num == '59') {
+				$scope.timeOn[2] = '00';
+				$scope.timeOn[0] = minTwoDigits(min + 1);
+			}
+			else {
+				$scope.timeOn[2] = minTwoDigits(num + 1);
+			}
 		}
 		$scope.timeOnJoined = $scope.timeOn.join('');
 	} // End changeTimeOn()
 	
-		$scope.changeTimeOff = function(value) {
+	
+	$scope.changeTimeOff = function(value) {
 		var num = parseInt($scope.timeOff[2]);
+		var min = parseInt($scope.timeOn[0]);
 		if (value === 'minus') {
-			$scope.timeOff[2] = num - 1;
+			if (num == '00') {
+				if (min == '00') {
+					$scope.timeOff[0] = minTwoDigits(min);
+				}
+				else {
+					$scope.timeOff[2] = '59';
+					$scope.timeOff[0] = minTwoDigits(min - 1);
+				}
+			}
+			else {
+				$scope.timeOff[2] = minTwoDigits(num - 1);
+			}
 		}
 		else if (value === 'plus') {
-			$scope.timeOff[2] = num + 1;
+			if (num == '59') {
+				$scope.timeOff[2] = '00';
+				$scope.timeOff[0] = minTwoDigits(min + 1);
+			}
+			else {
+				$scope.timeOff[2] = minTwoDigits(num + 1);
+			}
 		}
 		$scope.timeOffJoined = $scope.timeOff.join('');
 	} // End changeTimeOn()
 	
-	// Todo: Write function that will increase/decrease minute
 	
+	
+
+	
+	// Todo: Begin countdown on 'start'
 	
 	
 	

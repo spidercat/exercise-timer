432ab9fdc603272275d8eeadba0672f27135b28e
Laura <laura.kyle91@gmail.com>
2015-12-28 00:01:11 -0500

    Functionality almost done

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 var app = angular.module('tabataApp', []);
 app.controller('tabataAppCtrl', ['$scope', function($scope) {
 	
+	
+	
 	// Global variables (because I am a terrible person)
-	$scope.timeLeft = '00:20';
-	$scope.roundsLeft = '0';
-	//$scope.roundsTotal = '8';
+	//$scope.timeLeft = '00:20';
+	$scope.roundsLeft = 1;
+	$scope.breakLeft = ['00', ':', '10'];
+	$scope.breakLeftJoined = $scope.breakLeft.join('');
+	$scope.timeLeft = ['00', ':', '20'];
+	$scope.timeLeftJoined = $scope.timeLeft.join('');
+	
 	
 	// Option variables
-	$scope.rounds = '8';
+	$scope.rounds = 8;
 	$scope.timeOff = ['00', ':', '10'];
 	$scope.timeOn = ['00', ':', '20'];
+	
 	$scope.timeOnJoined = $scope.timeOn.join('');
 	$scope.timeOffJoined = $scope.timeOff.join('');
+	
+	//$scope.timeLeftJoined = $scope.timeOnJoined;
 	
 	
 	
@@ -22,7 +31,6 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 	
 	
 	$scope.changeRounds = function(value) {
-		// Hold parsed int
 		var numRounds = parseInt($scope.rounds);
 		if ((value === 'minus') && (numRounds != 0)) {
 			$scope.rounds = numRounds - 1;
@@ -60,6 +68,8 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 			}
 		}
 		$scope.timeOnJoined = $scope.timeOn.join('');
+		//$scope.originalTime.timeOn = $scope.timeOnJoined;
+		$scope.timeLeft = $scope.timeOnJoined;
 	} // End changeTimeOn()
 	
 	
@@ -90,15 +100,63 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 			}
 		}
 		$scope.timeOffJoined = $scope.timeOff.join('');
+		//$scope.originalTime.timeOff = $scope.timeOffJoined;
 	} // End changeTimeOn()
 	
 	
-	
+	// Todo: Set up for minutes
+	$scope.startClock = function() {
+		
+		if ($scope.roundsLeft <= $scope.rounds) {
+		
+			if ($('#timeLeft').hasClass('hidden')) {		
+				var breakLeftInterval = setInterval(function() {
+					var seconds = parseInt($scope.breakLeft[2]);
+					$scope.breakLeft[2] = minTwoDigits(seconds - 1);
+					$scope.breakLeftJoined = $scope.breakLeft.join('');
+					$scope.$apply();
+					if (seconds <= '00') {
+						$('#timeLeft').removeClass('hidden');
+						$('#breakLeft').addClass('hidden');
+						$scope.breakLeft = $scope.timeOff;
+						$scope.breakLeftJoined = $scope.breakLeft.join('');
+						console.log($scope.breakLeftJoined);
+						$scope.$apply;
+						clearInterval(breakLeftInterval);
+						// Reset break
+						$scope.startClock();
+					}
+				}, 500);
 
+			}
+
+			else {
+				var timeLeftInterval = setInterval(function() {
+					var seconds = parseInt($scope.timeLeft[2]);
+					$scope.timeLeft[2] = minTwoDigits(seconds - 1);
+					$scope.timeLeftJoined = $scope.timeLeft.join('');
+					$scope.$apply();
+					console.log($scope.timeLeft[2]);
+					if (seconds <= '00') {
+						$scope.roundsLeft = $scope.roundsLeft + 1;
+						$('#breakLeft').removeClass('hidden');
+						$('#timeLeft').addClass('hidden');
+						$scope.timeLeft = $scope.timeOn;
+						$scope.timeLeftJoined = $scope.timeLeft.join('');
+						console.log($scope.timeLeftJoined);
+						$scope.$apply;
+						clearInterval(timeLeftInterval);
+						// Reset break
+						$scope.startClock();
+					}
+				}, 500);
+			}
+			
+		}
+	}
 	
-	// Todo: Begin countdown on 'start'
+// Currently breaks after a couple of rounds. 00:0-1. Type issue?
 	
-	
-	
+//	 Fix them for if minute. Two-way bind break with option. Add for loop, minus round after each timeOn. Pause and clear buttons
 
 }]) // End controller

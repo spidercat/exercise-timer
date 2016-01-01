927bba4e1406a40e7eaa50f70e17d9899b881483
Laura <laura.kyle91@gmail.com>
2016-01-01 01:09:20 -0500

    Functionality done except for audio

--- src/app.js
+++ src/app.js
@@ -5,16 +5,16 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 	$scope.roundsLeft = 1;
 	
 	// Count down variables. See if I can remove values.
-	$scope.breakLeft = ['00', ':', '55'];
-	$scope.timeLeft = ['00', ':', '10'];
+	$scope.breakLeft = ['00', ':', '50'];
+	$scope.timeLeft = ['00', ':', '50'];
 	
 	$scope.breakLeftJoined = $scope.breakLeft.join('');
 	$scope.timeLeftJoined = $scope.timeLeft.join('');
 	
 	// Option variables. Get copied for tempArray.
 	$scope.rounds = 8;
-	$scope.timeOff = ['00', ':', '55'];
-	$scope.timeOn = ['00', ':', '10'];
+	$scope.timeOff = ['00', ':', '50'];
+	$scope.timeOn = ['00', ':', '50'];
 	
 	$scope.timeOnJoined = $scope.timeOn.join('');
 	$scope.timeOffJoined = $scope.timeOff.join('');
@@ -25,6 +25,38 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
   	return (n < 10 ? '0' : '') + n;
 	}
 	
+	  // Reduce time by one second
+	function subtractTime(time) {
+		var seconds = parseInt(time[2]);
+		var minutes = parseInt(time[0]);
+		if (seconds == '00') {
+				if (minutes == '00') {
+				  time[0] = minTwoDigits(minutes);
+				}
+				else {
+				  time[2] = '59';
+					time[0] = minTwoDigits(minutes - 1);
+				}
+			}
+			else {
+				time[2] = minTwoDigits(seconds - 1);
+			}
+	}
+	
+	// Increase time by one second
+	function addTime(time) {
+		var seconds = parseInt(time[2]);
+		var minutes = parseInt(time[0]);
+		if (seconds == '59') {
+			time[2] = '00';
+			time[0] = minTwoDigits(minutes + 1);
+		}
+		else {
+			time[2] = minTwoDigits(seconds + 1);
+		}
+	}
+	
+	
 	// User adjusted rounds value
 	$scope.changeRounds = function(value) {
 		var numRounds = parseInt($scope.rounds);
@@ -34,77 +66,40 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 		else if (value === 'plus') {
 			$scope.rounds = numRounds + 1;
 		}
-	} // End changeRounds()
+	} 
 	
-	// User adjusted time on value
+	
+	// User adjusted timeOn value
 	$scope.changeTimeOn = function(value) {
-		var seconds = parseInt($scope.timeOn[2]);
-		var minutes = parseInt($scope.timeOn[0]);
 		if (value === 'minus') {
-			if (seconds == '00') {
-				if (minutes == '00') {
-					$scope.timeOff[0] = minTwoDigits(minutes);
-				}
-				else {
-					$scope.timeOn[2] = '59';
-					$scope.timeOn[0] = minTwoDigits(minutes - 1);
-				}
-			}
-			else {
-				$scope.timeOn[2] = minTwoDigits(seconds - 1);
-			}
-		}
+			subtractTime($scope.timeOn);
+		}	
 		else if (value === 'plus') {
-			if (seconds == '59') {
-				$scope.timeOn[2] = '00';
-				$scope.timeOn[0] = minTwoDigits(minutes + 1);
-			}
-			else {
-				$scope.timeOn[2] = minTwoDigits(seconds + 1);
-			}
-		}
+			addTime($scope.timeOn);
+		}		
 		$scope.timeOnJoined = $scope.timeOn.join('');
 		var temp = $scope.timeOn.slice();
 		$scope.timeLeft = temp;
 		$scope.timeLeftJoined = $scope.timeOnJoined;
 		$scope.$apply;
-	} // End changeTimeOn()
+	} 
 	
-	// User adjusted time off value
+	// User adjusted timeOff value
 	$scope.changeTimeOff = function(value) {
-		var seconds = parseInt($scope.timeOff[2]);
-		var minutes = parseInt($scope.timeOn[0]);
 		if (value === 'minus') {
-			if (seconds == '00') {
-				if (minutes == '00') {
-					$scope.timeOff[0] = minTwoDigits(minutes);
-				}
-				else if (minutes != '00') {
-					$scope.timeOff[2] = '59';
-					$scope.timeOff[0] = minTwoDigits(minutes - 1);
-				}
-			}
-			else {
-				$scope.timeOff[2] = minTwoDigits(seconds - 1);
-			}
+			subtractTime($scope.timeOff);
 		}
 		else if (value === 'plus') {
-			if (seconds == '59') {
-				$scope.timeOff[2] = '00';
-				$scope.timeOff[0] = minTwoDigits(minutes + 1);
-			}
-			else {
-				$scope.timeOff[2] = minTwoDigits(seconds + 1);
-			}
+			addTime($scope.timeOff);
 		}
 		$scope.timeOffJoined = $scope.timeOff.join('');
 		var temp = $scope.timeOff.slice();
 		$scope.breakLeft = temp;
 		$scope.breakLeftJoined = $scope.timeOffJoined;
 		$scope.$apply;
-	} // End changeTimeOn()
+	} 
 	
-	
+
 	// Todo: Set up for minutes
 	$scope.startClock = function() {
 		$('#pause-button').removeClass('hidden');
@@ -113,50 +108,65 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 		
 			if ($('#timeLeft').hasClass('hidden')) {		
 				  breakLeftInterval = setInterval(function() {
-					var minutes = parseInt($scope.breakLeft[0]);
-					var seconds = parseInt($scope.breakLeft[2]);
-					if (seconds != 00) {
-						$scope.breakLeft[2] = minTwoDigits(seconds - 1);
-						$scope.breakLeftJoined = $scope.breakLeft.join('');
-						$scope.$apply();
-					}
-					else if (seconds == 00) {
-						$('#timeLeft').toggleClass('hidden');
-						$('#breakLeft').toggleClass('hidden');
-						$('#time-left').css('background-color', '#a5d6a7');
-						var tempArray = $scope.timeOff.slice();
-						$scope.breakLeft = tempArray;
-						$scope.breakLeftJoined = $scope.timeOffJoined;
-						$scope.$apply;
-						clearInterval(breakLeftInterval);
-						$scope.startClock();
-					}
+						var minutes = parseInt($scope.breakLeft[0]);
+						var seconds = parseInt($scope.breakLeft[2]);
+						if (seconds != 00) {
+							$scope.breakLeft[2] = minTwoDigits(seconds - 1);
+							$scope.breakLeftJoined = $scope.breakLeft.join('');
+							$scope.$apply();
+						}
+						else if ((seconds == 00) && (minutes != 00)) {
+								$scope.breakLeft[0] = minTwoDigits(minutes - 1);
+								$scope.breakLeft[2] = '59';
+								$scope.breakLeftJoined = $scope.breakLeft.join('');
+								$scope.$apply();
+							}
+						else {
+							$('#timeLeft').toggleClass('hidden');
+							$('#breakLeft').toggleClass('hidden');
+							$('#time-left').css('background-color', '#a5d6a7');
+							var tempArray = $scope.timeOff.slice();
+							$scope.breakLeft = tempArray;
+							$scope.breakLeftJoined = $scope.timeOffJoined;
+							$scope.$apply;
+							clearInterval(breakLeftInterval);
+							$scope.startClock();
+						}
+					
 				}, 200);
 
 			}
 
 			else {
 				  timeLeftInterval = setInterval(function() {
-					var seconds = parseInt($scope.timeLeft[2]);
-					if (seconds != 00) {
-						$scope.timeLeft[2] = minTwoDigits(seconds - 1);
-						$scope.timeLeftJoined = $scope.timeLeft.join('');
-						$scope.$apply();
-					}
-					else if (seconds == 00) {
-						$scope.roundsLeft = $scope.roundsLeft + 1;
-						$('#breakLeft').toggleClass('hidden');
-						$('#timeLeft').toggleClass('hidden');
-						$('#time-left').css('background-color', '#ef9a9a');
-					  // To hold copied array. Because apparently, assigning an array as a value
-						// creates a pointer. What the hell, JavaScript? Yes, this took me a day to solve.
-						var tempArray = $scope.timeOn.slice();
-						$scope.timeLeft = tempArray;
-						$scope.timeLeftJoined = $scope.timeOnJoined;
-						$scope.$apply;
-						clearInterval(timeLeftInterval);
-						// Reset break
-						$scope.startClock();
+						var seconds = parseInt($scope.timeLeft[2]);
+						var minutes = parseInt($scope.timeLeft[0]);
+						if (seconds != 00) {
+							$scope.timeLeft[2] = minTwoDigits(seconds - 1);
+							$scope.timeLeftJoined = $scope.timeLeft.join('');
+							$scope.$apply();
+						}
+						else if ((seconds == 00) && (minutes != 00)) {
+							$scope.timeLeft[0] = minTwoDigits(minutes - 1);
+							$scope.timeLeft[2] = '59';
+							$scope.timeLeftJoined = $scope.timeLeft.join('');
+							$scope.$apply();
+						}
+					
+						else {
+							$scope.roundsLeft = $scope.roundsLeft + 1;
+							$('#breakLeft').toggleClass('hidden');
+							$('#timeLeft').toggleClass('hidden');
+							$('#time-left').css('background-color', '#ef9a9a');
+							// To hold copied array. Because apparently, assigning an array as a value
+							// creates a pointer. What the hell, JavaScript? Yes, this took me a day to solve.
+							var tempArray = $scope.timeOn.slice();
+							$scope.timeLeft = tempArray;
+							$scope.timeLeftJoined = $scope.timeOnJoined;
+							$scope.$apply;
+							clearInterval(timeLeftInterval);
+							// Reset break
+							$scope.startClock();
 					}
 				}, 200);
 			}
@@ -197,6 +207,6 @@ app.controller('tabataAppCtrl', ['$scope', function($scope) {
 	}
 	
 	
-//	 Fix them for if minute.  sound. break to over 1min then back down broken.
+//	 Fix them for if minute.  sound.
 
 }]) // End controller

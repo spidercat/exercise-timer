eb0ceb640d0ab5e05708a538cd591f06cbf65e6a
Laura <laura.kyle91@gmail.com>
2016-10-09 20:31:53 -0400

    Added interval function for break timer

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
-var app = angular.module('tabataApp', []);
-app.controller('tabataAppCtrl', ['$scope', function ($scope) {
+//todo: create switch screens function
+
+var app = angular.module("tabataApp", []);
+app.controller("tabataAppCtrl", ["$scope", function ($scope) {
 	
 	// If running, mute option buttons?
 	
 	
 		
 		// Set default volume as on
-	$('#volume-switch').prop('checked', true);
+	$("#volume-switch").prop("checked", true);
 		
 		// Rounds object
 	$scope.rounds = {
@@ -24,10 +26,15 @@ app.controller('tabataAppCtrl', ['$scope', function ($scope) {
 		workTime: "00:20"
 	}
 	
+	$scope.timerStates = {
+		breakRunning: false,
+		workRunning: false
+	}
+	
 	
 	// Parse time string int integers
 	function parseTime(time) {
-		var time = time.split(':');
+		var time = time.split(":");
 		var SECONDS = parseInt(time[1]);
 		var MINUTES = parseInt(time[0]);
   	return [MINUTES, SECONDS];
@@ -35,14 +42,14 @@ app.controller('tabataAppCtrl', ['$scope', function ($scope) {
 	
 	// Put 0 if digit less than 10
 	function minTwoDigits(n) {
-		return (n < 10 ? '0' : '') + n;
+		return (n < 10 ? "0" : "") + n;
 	}
 	
 
 	// Adds or removes 1 second
 	$scope.changeTime = function (currentTime, deltaTime) {
 		var time = parseTime(currentTime);
-		if (time[1] === 0 && deltaTime === '-1') {
+		if (time[1] === 0 && deltaTime === "-1") {
 			var newTime = (minTwoDigits(time[0]) + ":" + minTwoDigits(time[1])).toString();
 			return newTime;	
 		}
@@ -56,14 +63,16 @@ app.controller('tabataAppCtrl', ['$scope', function ($scope) {
 		// if 60 or -1
 	};
 	
+	/* Functions to change the option numbers
+	================================================*/
+	
 	// Change the number of rounds
 	$scope.changeRounds = function(currentRounds, deltaRounds) {
-		if ($scope.rounds.totalRounds === 0 && deltaRounds === '-1') {
+		if ($scope.rounds.totalRounds === 0 && deltaRounds === "-1") {
 			return;
 		}
 		$scope.rounds.totalRounds = eval(currentRounds + deltaRounds);
 	}
-	
 	
 	// Could not find a way to pass property, so this is the hacky temporary solution
 	$scope.changeTimeOff = function(currentTime, deltaTime) {
@@ -78,7 +87,65 @@ app.controller('tabataAppCtrl', ['$scope', function ($scope) {
 		$scope.timerTimes.workTime = newTime;
 	}
 	
+	
+	
+	// Switch between break screen and work screen
+	function switchScreens(value) {
+		if (value === "toWork) {
+				
+		}
+		else {
+		}
+				
+	} // End switchScreens
 
+	// Start the timer
+	$scope.startClock = function() {
+		$("#pause-button").removeClass("hidden");
+		$("#start-button").addClass("hidden");
+		// If there is a round left
+		if ($scope.rounds.roundsLeft <= $scope.rounds.totalRounds) {
+			// Break timer
+			startInterval = setInterval(function() { 
+				if (!$scope.timerStates.workRunning) {
+					$scope.timerStates.breakRunning = true;
+					var newTime = $scope.changeTime($scope.timerTimes.breakTime, "-1");
+					$scope.timerTimes.breakTime = newTime;
+					$scope.$apply();
+					if (newTime === "00:00") {
+						switchScreens("toWork");	
+					}
+				}
+				else {
+					
+				}
+			
+		},1000);
+		
+			
+			
+			
+			
+		} // End if statement
+	}; // End startClock()
+	
+	
+	
+	$scope.pauseClock = function() {
+		$("#pause-button").addClass("hidden");
+		$("#start-button").removeClass("hidden");
+		clearInterval(startInterval);
+	}
+	
+	$scope.clear = function() {
+		clearInterval(startInterval);
+		$scope.timerTimes.breakTime = $scope.optionTimes.timeOff;
+		$scope.timerTimes.workTime = $scope.optionTimes.timeOn;
+	}
+	
+	
+	
+	
 	
 
 }]); // End controller
